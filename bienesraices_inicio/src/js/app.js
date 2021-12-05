b5b4f50c413233f71f10110d9011ce09f033ddfd
DavidEF1996 <59062590+DavidEF1996@users.noreply.github.com>
2021-12-05 14:20:09 -0500

    Nuevo diseño

--- bienesraices_inicio/src/js/app.js
+++ bienesraices_inicio/src/js/app.js
@@ -1,54 +1,53 @@
-document.addEventListener('DOMContentLoaded', function(){
-    //Este es el evento principal, aplicara todas las funciones js cuando el documento
-    //haya cargado toddos los html, css, etc
+document.addEventListener("DOMContentLoaded", function () {
+  //Este es el evento principal, aplicara todas las funciones js cuando el documento
+  //haya cargado toddos los html, css, etc
 
-    eventListeners();
-    darkMode();
-
+  eventListeners();
+  //  darkMode();
 });
 
-function eventListeners(){
+function eventListeners() {
+  //esta funcion da eventos a los diferentes elemetnso
+  const mobileMenu = document.querySelector(".mobile-menu"); //obtenemos el menu hamburguesa
+  mobileMenu.addEventListener("click", (e) => {
+    //le damos una accion de click y cuando esto pase llamamos a la funcion navegacionResponsive();
 
-    //esta funcion da eventos a los diferentes elemetnso
-    const mobileMenu = document.querySelector('.mobile-menu'); //obtenemos el menu hamburguesa
-    mobileMenu.addEventListener('click', (e)=>{ //le damos una accion de click y cuando esto pase llamamos a la funcion navegacionResponsive();
-
-      navegacionResponsive();
-    });
+    navegacionResponsive();
+  });
 }
 
-function navegacionResponsive(){
-    
-    const navegacion  = document.querySelector('.navegacion'); //obtenemos ahora el menu de navegacion
-    
-    if ( navegacion.classList.contains('mostrar')){ //si navegacion contiene la etiqueta de mostrar la va a remover, esto para que oculte el menu
-        navegacion.classList.remove('mostrar');
-    }else{
-        navegacion.classList.add('mostrar'); //caso contrario la va a agregar para que muestre el menu
-    }
+function navegacionResponsive() {
+  const navegacion = document.querySelector(".navegacion"); //obtenemos ahora el menu de navegacion
+
+  if (navegacion.classList.contains("mostrar")) {
+    //si navegacion contiene la etiqueta de mostrar la va a remover, esto para que oculte el menu
+    navegacion.classList.remove("mostrar");
+  } else {
+    navegacion.classList.add("mostrar"); //caso contrario la va a agregar para que muestre el menu
+  }
 }
 
-function darkMode(){
-    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+function darkMode() {
+  const prefiereDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
 
-    // console.log(prefiereDarkMode.matches);
+  // console.log(prefiereDarkMode.matches);
 
-    if(prefiereDarkMode.matches) {
-        document.body.classList.add('dark-mode');
+  if (prefiereDarkMode.matches) {
+    document.body.classList.add("dark-mode");
+  } else {
+    document.body.classList.remove("dark-mode");
+  }
+
+  prefiereDarkMode.addEventListener("change", function () {
+    if (prefiereDarkMode.matches) {
+      document.body.classList.add("dark-mode");
     } else {
-        document.body.classList.remove('dark-mode');
+      document.body.classList.remove("dark-mode");
     }
+  });
+  const variableDark = document.querySelector(".dark-mode-boton");
 
-    prefiereDarkMode.addEventListener('change', function() {
-        if(prefiereDarkMode.matches) {
-            document.body.classList.add('dark-mode');
-        } else {
-            document.body.classList.remove('dark-mode');
-        }
-    });
-    const variableDark = document.querySelector('.dark-mode-boton');
-
-    variableDark.addEventListener('click', ()=>{
-        document.body.classList.toggle('dark-mode');
-    });
+  variableDark.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+  });
 }
