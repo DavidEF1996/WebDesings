291a6ba7bea1a37109484263050cf77f9d9d51d5
DavidEF1996 <59062590+DavidEF1996@users.noreply.github.com>
2021-12-21 00:01:02 -0500

    Galeria para los licores

--- bienesraices_inicio/anuncio.php
+++ bienesraices_inicio/anuncio.php
@@ -1,33 +1,40 @@
 <?php
 $id = $_GET['id'];
-$id = filter_var($id, FILTER_VALIDATE_INT);
+
+
 
 if(!$id){
     header('Location: /');
 }
 
+$ron = "ron";
+$contador = 1;
 
 require 'includes/config/database.php';
 $db = conectarBD();
 
-$query ="SELECT * FROM propiedades where idPropiedades = ${id} ";
+$query ="SELECT * FROM ${id}";
+
 
 $resultado = mysqli_query($db,$query);
 if($resultado->num_rows===0){
     header('Location: /');
 }
-$propiedad = mysqli_fetch_assoc($resultado);
+
+
 
 require 'includes/funciones.php';
 incluirTemplate('header');  
 
 ?>
     
-    <main class="contenedor seccion contenido-centrado"> <!--Se crea el contenedor principal y usamos seccion para tener margin y padding, este esta creado en utilidades-->
-    <h1><?php echo $propiedad['tituloPropiedades']?></h1>
-
+    <div class="contenedor-anuncios contenedor "> <!--Se crea el contenedor principal y usamos seccion para tener margin y padding, este esta creado en utilidades-->
+    
+        
+      <?php while ($propiedad = mysqli_fetch_assoc($resultado)): ?>
 
         <div class="imagen-anuncio">
+        <h1><?php echo $propiedad['desc_lic']?></h1>
             <picture> <!--Esta propiedad permite cargar imagenes en diferente formato segun soporte el navegador-->
                 <source srcset="build/img/destacada.webp" type="image/webp">
                 <source srcset="build/img/destacada.jpg" type="image/jpg">
@@ -36,18 +43,21 @@ incluirTemplate('header');
 
             <div id="galeria">
  <div id="galeria_imagen">
- <img id="imgGaleria" src="build/img/anuncio1.webp" /></div>
+ <img id="imgGaleria <?php echo $contador ?>" src="build/img/anuncio1.webp" /></div>
  <div id="galeria_miniaturas">
- <img class="miniatura" onclick="javascript:document.getElementById('imgGaleria').src=this.src;" src="build/img/anuncio1.webp" />
- <img class="miniatura" onclick="javascript:document.getElementById('imgGaleria').src=this.src;" src="build/img/anuncio2.webp" />
- <img class="miniatura" onclick="javascript:document.getElementById('imgGaleria').src=this.src;" src="build/img/anuncio3.webp" />
+ <img class="miniatura" onclick="javascript:document.getElementById('imgGaleria <?php echo $contador ?>').src=this.src;" src="build/img/anuncio1.webp" />
+ <img class="miniatura" onclick="javascript:document.getElementById('imgGaleria <?php echo $contador ?>').src=this.src;" src="build/img/anuncio2.webp" />
+ <img class="miniatura" onclick="javascript:document.getElementById('imgGaleria <?php echo $contador ?>').src=this.src;" src="build/img/anuncio3.webp" />
  </div>
  </div>
-            <p class="precio">$<?php echo $propiedad['precioPropiedades']?></p>
+            <p class="precio">$<?php echo $propiedad['precio_lic']?></p>
            
-            <p><?php echo $propiedad['descripcionPropiedades'] ?> </p>
+            <p><?php echo $propiedad['precio_lic'] ?> </p>
         </div>
-    </main>
+        <?php $contador++; ?>
+        <?php endwhile ?>
+      </div>
+    
 
 
     <?php
--- bienesraices_inicio/includes/config/database.php
+++ bienesraices_inicio/includes/config/database.php
@@ -1,6 +1,6 @@
 <?php
 function conectarBD():mysqli{
-    $db = mysqli_connect('localhost', 'root', 'cuenca', 'bienes_raices');
+    $db = mysqli_connect('localhost', 'root', 'cuenca', 'hm_eventos');
     if(!$db){
         echo "Error en la conexion";
         exit;
