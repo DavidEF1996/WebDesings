d07a64b323f8cd6ffc13865aae760eb67d822aad
DavidEF1996 <59062590+DavidEF1996@users.noreply.github.com>
2022-01-03 22:49:45 -0500

    listar terminado

--- bienesraices_inicio/admin/index.php
+++ bienesraices_inicio/admin/index.php
@@ -1,5 +1,22 @@
 <?php
 
+$tipoProductoSeleccionado = $_GET['id']??'licores';
+$id_eliminar = $_POST['id_eliminar'];
+var_dump($id_eliminar);
+
+require '../includes/config/database.php';
+$db = conectarBD();
+
+$query = "Select * from $tipoProductoSeleccionado";
+
+$resultado = mysqli_query($db, $query);
+$aux = substr($tipoProductoSeleccionado,0 ,3);
+
+if($_SERVER['REQUEST_METHOD']==='POST'){
+   
+}
+
+
 $mensaje = $_GET['registrado']??null;
 
 require '../includes/funciones.php';
@@ -15,11 +32,62 @@ incluirTemplate('header');
         <p class="alerta exito">Producto Insertado Correctamente</p>
         <?php endif;?>
 
+        <div class="menuProductos">
+  
+  <form method="GET" name="pasteleria" action="/admin/index.php">
+          <input type="hidden" name="id" value="pasteleria">
+          <input type="submit"  value="Pasteleria" >
+      </form>
+        <form method="GET" name="licores" action="/admin/index.php">
+          <input type="hidden" name="id" value="licores">
+          <input type="submit"  value="Licores" >
+      </form>
+      <form method="POST">
+                                    <input type="hidden" name="id_eliminar" value="hola">
+                                     <input type="submit" class="boton-rojo-block" value="Eliminar">
+                            </form>
+                      
+</div>
+
+
         <table class="productos">
+            <thead>
+                <tr>
+
+                <th>ID</th>
+                <th>Producto</th>
+                <th>Precio</th>
+                <th>Unidad de Medida</th>
+                <th>Acciones</th>
+                </tr>
+
+                <tbody>
+                    <?php while($propiedad = mysqli_fetch_assoc($resultado)): ?>
+                    <tr>
+                        <td><?php echo($propiedad['id_'.$aux])?></td>
+                        <td><?php echo ($propiedad['desc_'.$aux])?></td>
+                        <td>$<?php echo ($propiedad['precio_'.$aux])?></td>
+                        <td><?php echo ($propiedad['unidad_medida_'.$aux]) ?></td>
+                        <td>
+                            <form method="POST">
+                                    <input type="hidden" name="id_eliminar" value="hola">
+                                     <input type="submit" class="boton-rojo-block" value="Eliminar">
+                            </form>
+                      
+                       
+                        </td>
+                        <td>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                        </td>
+                    </tr>
+                    <?php endwhile;?>
+                </tbody>
+            </thead>
             
         </table>
 </main>
 
 <?php
+mysqli_close($db);
 incluirTemplate('footer');
 ?>
